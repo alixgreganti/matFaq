fa68c464fc4e2e6882e0487c0e5436e6b5a2ab9e
Jason Rector <jrector@auxiliumgroup.ca>
2023-01-30 10:25:23 -0500

    formatting additions

--- knowledgebase/src/app/faq-content/faq-content.component.ts
+++ knowledgebase/src/app/faq-content/faq-content.component.ts
@@ -15,33 +15,25 @@ interface FAQ {
   template: `
     <div class="row">
       <div class="col-sm-2" id="sideNav">
-      <ng-template [ngIf]="dataLoaded | async">
-      <div *ngFor="let category of categories">
-        <ng-template [ngIf]="filter == category" [ngIfElse]="inactive"]>
-
+        <ng-template [ngIf]="dataLoaded | async">
+          <div *ngFor="let category of categories">
+            <h3 (click)="filterFaq(category)" class="category">
+              {{ category }}
+            </h3>
+          </div>
         </ng-template>
-        
-        <ng-component #inactive>
-          <h3 (onclick)="filterFaq(category)" class="category">{{category}}</h3>
-        </ng-component>
-
-      </div>
-      </ng-template>
-
       </div>
 
       <div class="col-sm" id="content">
         <ng-template [ngIf]="dataLoaded | async" [ngIfElse]="loading" ]>
-          <div *ngFor="let question of faqs">
-            <div class="card" style="width: 80%;">
+          <div *ngFor="let question of filteredFaqs">
+            <div class="card" style="width: 100%;">
               <div class="card-body">
                 <h5 class="card-title">{{ question.Title }}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">
                   {{ question.Category }}
                 </h6>
                 <p class="card-text">{{ question.Description }}</p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
               </div>
             </div>
           </div>
@@ -58,25 +50,23 @@ interface FAQ {
   styleUrls: ['./faq-content.component.scss'],
 })
 export class FaqContentComponent implements OnInit {
+  dataLoaded: Promise<Boolean> = Promise.resolve(false); //Keeps track of when the data is loaded from source
   categories: string[] = [];
 
   faqs: Array<FAQ> = [];
-  filteredFaqs: Array<FAQ> = [];
-
-  dataLoaded: Promise<Boolean> = Promise.resolve(false);
-
-  filter: string = "none"
+  filteredFaqs = this.faqs; //by default there is no filter applied
 
   constructor() {}
 
   ngOnInit(): void {
-    this.categories.push("All")
+    this.categories.push('All'); //Adds "All" category
 
     getData().then((data) => {
       console.log('Getting data...');
       data.rows.forEach((item: FAQ) => {
         // iterate through the row data
         this.faqs.push({
+          //adds the data taken to faqs using the FAQ interface
           id: item.id,
           Title: item.Title,
           Category: item.Category,
@@ -84,8 +74,8 @@ export class FaqContentComponent implements OnInit {
           created: item.created,
         });
 
-        if(!this.categories.includes(item.Category)){
-          this.categories.push(item.Category)
+        if (!this.categories.includes(item.Category)) {
+          this.categories.push(item.Category);
         }
       });
       console.log('Finished getting FAQs');
@@ -93,11 +83,13 @@ export class FaqContentComponent implements OnInit {
     });
   }
 
-  filterFaq(category: string){
-    if(category=="All"){
-      this.filter = "none"
+  filterFaq(category: string) {
+    if (category == 'All') {
+      //Show all
+      this.filteredFaqs = this.faqs;
     } else {
-      this.filter = category
+      //Set filteredFaqs equal to the faq, filter the faqs by the selected category
+      this.filteredFaqs = this.faqs.filter((faq) => faq.Category === category);
     }
   }
 }
